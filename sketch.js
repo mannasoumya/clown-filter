813e67e311bbe1bc95446f7e0b17f837291cd8df
mannasoumya <sm323265@gmail.com>
2020-07-09 22:46:03 +0530

    added opacity slider

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ let lefteyeX = 0;
 let lefteyeY = 0;
 let righteyeX = 0;
 let righteyeY = 0;
-let c, msg, button2, r_slider, g_slider, b_slider;
+let c, msg, button2, r_slider, g_slider, b_slider, opacity;
 function setup() {
   c = createCanvas(600, 400);
   c.parent("skk");
@@ -20,18 +20,22 @@ function setup() {
   msg.html("Clown and Spectacles Emoji<br></br>");
   button.mousePressed(savecanvas);
   button.style("margin", "0 5px");
-  r_slider = createSlider(0, 255, 255, 10);
+  r_slider = createSlider(0, 255, 128, 10);
   r_slider.position(20, 420);
   g_slider = createSlider(0, 255, 0, 10);
   g_slider.position(120, 420);
-  b_slider = createSlider(0, 255, 30, 10);
+  b_slider = createSlider(0, 255, 128, 10);
   b_slider.position(220, 420);
+  opacity = createSlider(50, 255, 100, -10);
+  opacity.position(320, 420);
   r_slider.style("width", "80px");
   r_slider.style("margin", "0 5px");
   g_slider.style("width", "80px");
   g_slider.style("margin", "0 5px");
   b_slider.style("width", "80px");
   b_slider.style("margin", "0 5px");
+  opacity.style("width", "80px");
+  opacity.style("margin", "0 5px");
 }
 function savecanvas() {
   let name = floor(random(1, 5000)).toString();
@@ -71,14 +75,16 @@ function draw() {
   text("green", g_slider.x + g_slider.width / 3, 370);
   fill(0, 0, 255);
   text("blue", b_slider.x + b_slider.width / 2.7, 370);
-
+  fill(255);
+  text("opacity", opacity.x + opacity.width / 4, 370);
+  
   strokeWeight(1);
   stroke(255);
 
   fill(255, 0, 30);
   ellipse(noseX, noseY, d * 0.8);
   // fill(140,0,140,100);
-  fill(r_slider.value(), g_slider.value(), b_slider.value(), 100);
+  fill(r_slider.value(), g_slider.value(), b_slider.value(), opacity.value());
   stroke(255);
   ellipse(lefteyeX + offset, lefteyeY, d * 1.2);
   ellipse(righteyeX - offset, righteyeY, d * 1.2);
